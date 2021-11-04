f392af3e5fb937d8e422655c1b6fc701fb7e1178
CarterSnich <jmestreraclosa@gmail.com>
2021-11-04 13:15:30 +0800

    change emgithub color scheme to dark

--- embeds.php
+++ embeds.php
@@ -1,10 +1,12 @@
 <?php
 
+$color_scheme = 'dark';
+
 $embeds = array(
-    'req1' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq1.php&style=github&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
-    'req2' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq2.php&style=github&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
-    'req3' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq3.php&style=github&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
-    'req4' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq4.php&style=github&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>'
+    'req1' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq1.php&style='.$color_scheme.'&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
+    'req2' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq2.php&style='.$color_scheme.'&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
+    'req3' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq3.php&style='.$color_scheme.'&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>',
+    'req4' => '<script src="https://emgithub.com/embed.js?target=https%3A%2F%2Fgithub.com%2FCarterSnich%2FIPT%2Fblob%2Fmaster%2Freq4.php&style='.$color_scheme.'&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on"></script>'
 );
 
 ?>
